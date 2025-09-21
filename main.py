3c5565bc8a762f8ea6b6e5085cacf10a41e07da7
Nico <nb287@uni-rostock.de>
2025-09-21 20:20:41 +0200

    added timestamp

--- main.py
+++ main.py
@@ -64,7 +64,8 @@ async def presence_task():
 def create_games_overview_embed(games, timestamp_format="F", show_empty=False, show_outdated=False):
     embed = discord.Embed(
         title="Combined Arms Games - " + create_current_discord_timestamp(timestamp_format),
-        color=discord.Color.purple()
+        color=discord.Color.purple(),
+        timestamp=datetime.datetime.utcnow()
     )
 
     # Filter for Combined Arms games
